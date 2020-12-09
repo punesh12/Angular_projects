4bc5a64f7d0b338c19af4452414e6f650769a60c
punesh12 <30601775+punesh12@users.noreply.github.com>
2020-12-09 10:43:48 +0530

    Add files via upload

---
+++ Covid-19 Data/js/myScript.js
@@ -0,0 +1,39 @@
+const URL='https://covid19.mathdro.id/api';
+
+
+let app= angular.module("MyApp",[]);
+
+app.controller('myCtrl',($scope,$http)=>{
+    //this is controller
+    $scope.name="COVID-19 Data";
+
+    //calling API
+
+    $http.get(URL).then((response)=>{
+        console.log(response.data);
+
+        $scope.all_data= response.data;
+    },(error)=>{
+        console.log(error);
+    });
+
+    //get Country data
+    $scope.get_c_data= ()=>{
+        let Country= $scope.c;
+
+        if(Country == "")
+        {
+            $scope.c_data= undefined;
+            return;
+        }
+
+        $http.get(`${URL}/countries/${Country}`).then((response)=>{
+            console.log(response.data)
+            $scope.c_data= response.data;
+
+        },(error)=>{
+            console.log(error );
+        })
+
+    }
+})
